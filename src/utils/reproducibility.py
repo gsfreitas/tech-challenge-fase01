abd19d996aade4df96b1ded08019ad4d1c1075e7
diego skieresz <diegoskieresz@gmail.com>
2026-04-11 19:17:07 -0300

    chore: configura ambiente inicial e pyproject para eda

---
+++ src/utils/reproducibility.py
@@ -0,0 +1,10 @@
+import os
+import random
+
+import numpy as np
+
+
+def set_global_seed(seed: int = 42) -> None:
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
